3b042997382d5415cf8d3448c6264dc3cf51191b
JackMilner1 <jackellismilner@gmail.com>
2026-02-14 12:43:29 +0000

    exam input validation

--- src/main/java/com/jackmilner/gradecalc/AddExamUIController.java
+++ src/main/java/com/jackmilner/gradecalc/AddExamUIController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Label;
@@ -24,30 +23,92 @@ public class AddExamUIController implements Initializable {
     private TextField examName;
     @FXML
     private Button addExamButton;
+    @FXML
+    private TextField yourMarkField;
+    @FXML
+    private TextField maxMarkField;
+    @FXML
+    private TextField examWeightingField;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
-    @FXML
-    private void addExam(ActionEvent event) {
-        String name = examName.getText(); // name of new exam
-        
-        // setting up new fixed label for exam
-        
+    private Label createExamLabel(String nameOfExam) {
         Label nameLabel = new Label();
-        nameLabel.setText(name);
+        nameLabel.setText(nameOfExam);
         nameLabel.setFont(new Font("System",18.0));
         nameLabel.setMaxWidth(Double.MAX_VALUE);
         VBox.setMargin(nameLabel, new Insets(0,10,0,10));
         nameLabel.setTextFill(new Color(1.0, 1.0, 1.0, 1.0));
         nameLabel.setTextAlignment(TextAlignment.LEFT);
-        // adding to ui tree
+        return nameLabel;
+    }
+    
+    private boolean validMarks() {
+        return false;
+    }
+    
+    private boolean validWeight() {
+        return false;
+    }
+    
+    private boolean validIntegerField(String fieldText, boolean isRequired) { // used to validate fields so they can be assumed as integers in addExam()
+
+        if (fieldText.length() < 1 & isRequired) {
+            System.out.println("this field must not be empty!");
+            return false;
+        } 
         
-        examUI.getChildren().remove(addExamButton); // removes add button 
+        for (int i = 0; i < fieldText.length(); i++) {
+            String newCharacter = "" + fieldText.charAt(i);
+
+            if (!newCharacter.matches("[0-9]")) {
+                System.out.println(fieldText + " isnt valid | char : " + newCharacter);
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    @FXML
+    private void addExam(ActionEvent event) {
+
+        String name = examName.getText(); // name of new exam
+
+        // fields every new exam contains
+        String currentMark = yourMarkField.getText();
+        String maxMark = maxMarkField.getText();
+        String weight = examWeightingField.getText();
+        
+        boolean nameEntered = name.length() > 0;
+        boolean validFields = validIntegerField(currentMark,false) & validIntegerField(maxMark,true) & validIntegerField(weight,true); 
+        
+        // input validation
+        
+        if (!nameEntered) {
+            System.out.println("name must be entered");
+            return;
+        }
+        
+        if (!validFields) {
+            return;
+        }
+        
+        // if reached then all inputs are valid and ui can be created
+        
+        examWeightingField.setText(weight + "%"); // adds % sign to make it more clear its percentage weighting
+        Label newExam = createExamLabel(name);
+
+        // adding new exam to ui
+
+        examUI.getChildren().remove(addExamButton); 
         examUI.getChildren().remove(examName);
-        examUI.getChildren().add(0, nameLabel);
+        examUI.getChildren().add(0, newExam);
+        
+        
     }
 
 }
